6c0bc3a2f25180da3ee85408145de63f322fc859
hvtpham <30331020+hvtpham@users.noreply.github.com>
2018-07-23 21:33:45 -0400

    Added resume tab back in

--- includes/navigation.js
+++ includes/navigation.js
@@ -1,7 +1,7 @@
 function displayNavigation() {
   document.write("<div id='pagetitle'><h1>Hong Van Pham<\/h1><\/div>");
   document.write("<div id='nav'>");
-  document.write("<a href='index.html'>about</a> | <a href='publications.html'>publications</a> | <a href='contact.html'>contact</a>");
+  document.write("<a href='index.html'>about</a> | <a href="https://drive.google.com/file/d/1oZRrpuq2_6hsSjGmbouZiaek-RyDqv_H/view?usp=sharing">resume</a> | <a href='publications.html'>publications</a> | <a href='contact.html'>contact</a>");
   document.write("<\/div>");
 }
 
