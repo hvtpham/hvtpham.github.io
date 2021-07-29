188682616dcca76d153781a1df783d54d83ddd83
hvtpham <30331020+hvtpham@users.noreply.github.com>
2021-07-29 00:04:01 -0400

    Update navigation.js

--- includes/navigation.js
+++ includes/navigation.js
@@ -1,7 +1,7 @@
 function displayNavigation() {
   document.write("<div id='pagetitle'><h1>Hong Van Pham<\/h1><\/div>");
   document.write("<div id='nav'>");
-  document.write("<a href='index.html'>about</a> | <a href='https://goo.gl/3xPVqW' target='_blank'>resume</a> | <a href='portfolio.html'>portfolio</a> | <a href='contact.html'>contact</a>");
+  document.write("<a href='index.html'>about</a> | <a href='portfolio.html'>portfolio</a> | <a href='contact.html'>contact</a>");
   document.write("<\/div>");
 }
 
